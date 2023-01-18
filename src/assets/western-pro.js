fe9f2b0e156ff9cbc07e4dcccf4b820b9852e967
Ruchira <sewminiranasinghe111@gmail.com>
2023-01-18 17:56:20 +0530

    odel table change

--- src/assets/western-pro.js
+++ src/assets/western-pro.js
@@ -6,6 +6,8 @@ function WesternProContentFunction()
   //background color
   document.getElementById("westernContainer").style.backgroundColor = "#083C5A";
   document.getElementById("companyContainer").style.backgroundColor = "#25A55F";
+  document.getElementById("odelHeading").style.backgroundColor = "#FE9801";
+  document.getElementById("odelData").style.backgroundColor = "#F0EEB1";
 
   //change font color
   document.getElementById("westernMainTopic").style.color = "#E4F4FD";
